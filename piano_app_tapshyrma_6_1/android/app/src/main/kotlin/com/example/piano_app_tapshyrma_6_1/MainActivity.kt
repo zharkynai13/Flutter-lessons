37e566890a8f84dd7d45bf9cc7ec1a8a5530ecfa
Zharkynai <zharkynai.shabdanbekova.kk@gmail.com>
2023-04-11 05:44:48 -0700

    add piano app

---
+++ piano_app_tapshyrma_6_1/android/app/src/main/kotlin/com/example/piano_app_tapshyrma_6_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.piano_app_tapshyrma_6_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ piano_app_tapshyrma_6_1/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+ 
+        primarySwatch: Colors.blue,
+      ),
+      home: const DemoScreen(),
+    );
+  }
+}
+ class DemoScreen extends StatefulWidget {
+  const DemoScreen({super.key});
+
+  @override
+  State<DemoScreen> createState() => _DemoScreenState();
+}
+
+class _DemoScreenState extends State<DemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Demo Screen') ,
+      ),
+
+    );
+  }
+}
