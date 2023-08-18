63f1cd059c53116ab9529280c2f9f49c8db30dff
zharkynaishabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-08-18 14:41:45 +0600

    finished zharkynay app

---
+++ dart_for_and_loop/bin/dart_for_and_loop.dart
@@ -0,0 +1,5 @@
+import 'package:dart_for_and_loop/dart_for_and_loop.dart' as dart_for_and_loop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_for_and_loop.calculate()}!');
+}
---
+++ dart_for_and_loop/test/dart_for_and_loop_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_for_and_loop/dart_for_and_loop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
